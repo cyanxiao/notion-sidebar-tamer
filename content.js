6ac58e1090bfcb55fefbf070815610ee9a83ccc9
Chell <im.chell@outlook.com>
2023-06-14 21:50:06 +0200

    first commit

---
+++ content.js
@@ -0,0 +1,26 @@
+const observer = new MutationObserver((mutationsList, observer) => {
+  // Check if "notion-sidebar" class is present in the target element
+  const targetElement = document.querySelector(".notion-sidebar");
+  if (targetElement && targetElement.classList.contains("notion-sidebar")) {
+    const sidebar = document.getElementsByClassName("notion-sidebar")[0];
+    setTimeout(
+      () => {
+        if (sidebar && sidebar.style) {
+          sidebar.style.display = JSON.parse(
+            localStorage.getItem("LRU:KeyValueStore2:sidebar")
+          )["value"]["expanded"]
+            ? "block"
+            : "none";
+        }
+      },
+      JSON.parse(localStorage.getItem("LRU:KeyValueStore2:sidebar"))["value"][
+        "expanded"
+      ]
+        ? 0
+        : 500
+    );
+  }
+});
+
+// Start observing changes to the target element and its descendants
+observer.observe(document.body, { subtree: true, childList: true });

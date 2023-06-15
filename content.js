b8c398f5e93bf695668dba9996a422dbc7f55380
Chell <im.chell@outlook.com>
2023-06-15 20:12:40 +0200

    fix bugs

--- content.js
+++ content.js
@@ -4,9 +4,9 @@ const sidebarInitObserver = new MutationObserver((mutationsList, observer) => {
     observer.disconnect();
     const expandedObserver = new MutationObserver(expandedObserverCallback);
     expandedObserver.observe(targetElement, {
-      subtree: true,
-      childList: true,
-      attributes: false,
+      subtree: false,
+      childList: false,
+      attributes: true,
     });
   }
 });
@@ -15,22 +15,12 @@ let previousExpanded = null;
 
 function expandedObserverCallback(mutationsList, observer) {
   const targetElement = document.querySelector(".notion-sidebar");
-  if (targetElement) {
-    const sidebar = targetElement;
-    const sidebarData = JSON.parse(
-      localStorage.getItem("LRU:KeyValueStore2:sidebar")
-    );
-    const expanded = sidebarData["value"]["expanded"];
-
-    if (expanded !== previousExpanded) {
-      requestAnimationFrame(() => {
-        if (sidebar && sidebar.style) {
-          sidebar.style.display = expanded ? "block" : "none";
-        }
-      });
-
-      previousExpanded = expanded;
-    }
+  const sidebarData = JSON.parse(
+    localStorage.getItem("LRU:KeyValueStore2:sidebar")
+  );
+  const expanded = sidebarData["value"]["expanded"];
+  if (expanded !== previousExpanded) {
+    targetElement.style.display = expanded ? "block" : "none";
   }
 }
 

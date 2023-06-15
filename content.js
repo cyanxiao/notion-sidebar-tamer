2bb3bae3440920df1b5c14136e7d16b0892355a6
Chell <im.chell@outlook.com>
2023-06-15 11:16:36 +0200

    improve performance

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 const sidebarInitObserver = new MutationObserver((mutationsList, observer) => {
-  // Check if "notion-sidebar" class is present in the target element
   const targetElement = document.querySelector(".notion-sidebar");
-  if (targetElement && targetElement.classList.contains("notion-sidebar")) {
+  if (targetElement) {
     observer.disconnect();
     const expandedObserver = new MutationObserver(expandedObserverCallback);
     expandedObserver.observe(targetElement, {
@@ -14,26 +13,18 @@ const sidebarInitObserver = new MutationObserver((mutationsList, observer) => {
 
 function expandedObserverCallback(mutationsList, observer) {
   const targetElement = document.querySelector(".notion-sidebar");
-  if (targetElement && targetElement.classList.contains("notion-sidebar")) {
-    const sidebar = document.getElementsByClassName("notion-sidebar")[0];
-    setTimeout(
-      () => {
-        if (sidebar && sidebar.style) {
-          sidebar.style.display = JSON.parse(
-            localStorage.getItem("LRU:KeyValueStore2:sidebar")
-          )["value"]["expanded"]
-            ? "block"
-            : "none";
-        }
-      },
-      JSON.parse(localStorage.getItem("LRU:KeyValueStore2:sidebar"))["value"][
-        "expanded"
-      ]
-        ? 0
-        : 500
+  if (targetElement) {
+    const sidebar = targetElement;
+    const sidebarData = JSON.parse(
+      localStorage.getItem("LRU:KeyValueStore2:sidebar")
     );
+    const expanded = sidebarData["value"]["expanded"];
+    requestAnimationFrame(() => {
+      if (sidebar && sidebar.style) {
+        sidebar.style.display = expanded ? "block" : "none";
+      }
+    });
   }
 }
 
-// Start observing changes to the target element and its descendants
 sidebarInitObserver.observe(document.body, { subtree: true, childList: true });
